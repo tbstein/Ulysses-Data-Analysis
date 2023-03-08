4c753357934c8906590be844de576316e4678bd7
GOESTERN-0455813 <tobias.stein01@c084.physikcip.uni-goettingen.de>
2023-03-08 13:17:21 +0100

    Succesfully implemented spiceypy. Removed old beta

--- Ulysses Visualisation/DustDistriution.py
+++ Ulysses Visualisation/DustDistriution.py
@@ -5,67 +5,101 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from indices_dict import indices
+import spiceypy as spice
+
+spice_path = '../../spice/'
+spice.furnsh(spice_path + "naif0012.tls")
+one_year_et = spice.str2et('01-001T00:00')-spice.str2et('00-001T00:00')
 
 first_data_column = 3
 last_data_column = 34
 
 min_quality_flag = 2
-all_lines = True
+all_lines = False
+bins = 10
+PlottedQuantity = 'solar_lon'
 
 used_cols = (i for i in range(first_data_column, last_data_column))
 
-
 index = []
-date = []
-time_of_day = []
+time = []
 with open('Ulysses_Data_File_Cleaned.txt') as cleaned_ulysses_data:
     for count, line in enumerate(cleaned_ulysses_data):
-        if count > indices['first_data_line']:
+        if count >= indices['first_data_line']:
             line = line.split()
             index.append(line[indices['index']])
-            date.append(line[indices['date']])
-            time_of_day.append(line[indices['time_of_day']])
+            time.append(line[indices['date']]+'T'+line[indices['time_of_day']])
 
 index = np.array(index)
-year = [date[i].split('-')[0] for i in range(len(date))]
-day = [date[i].split('-')[1] for i in range(len(date))]
-hour = [time_of_day[i].split(':')[0] for i in range(len(time_of_day))]
-minute = [time_of_day[i].split(':')[1] for i in range(len(time_of_day))]
 
-for i in range(len(year)):
-    if year[i][0] == '9':
-        year[i] = '19' + year[i]
-    if year[i][0] == '0':
-        year[i] = '20' + year[i]
-
-year = np.array([int(year[i]) for i in range(len(year))])
-day = np.array([int(day[i]) for i in range(len(day))])
-hour = np.array([int(hour[i]) for i in range(len(hour))])
-minute = np.array([int(minute[i]) for i in range(len(minute))])
-
-passed_years = np.array([year[i]-year[0] for i in range(len(year))])
-
-def calculate_passed_time_in_minutes(passed_years: int, day: int, hour: int, minute: int) -> int:
-    return minute+60*hour+60*24*day+60*24*365*passed_years
-
-time = calculate_passed_time_in_minutes(passed_years, day, hour, minute)
+time = spice.str2et(time)
 
 ulysses_data = np.loadtxt('Ulysses_Data_File_Cleaned.txt', delimiter = ' ', skiprows = indices['first_data_line'], usecols = used_cols)
+
+ulysses_data = np.concatenate((np.transpose(np.array([time])), ulysses_data), axis = 1)
 
 if all_lines:
     start_index = 0
     end_index = len(ulysses_data)
 else:
-    start_index = 2000
-    end_index = start_index + 1000
+    start_et = spice.str2et('90-001T00:00')
+    end_et = start_et+one_year_et
+    start_index = np.argmin(np.abs(time-start_et))
+    end_index = np.argmin(np.abs(time-end_et))
 
-ulysses_data_without_999 = []
-for i in range(start_index, end_index):
-    condition = ulysses_data[i,indices['rotation_angle_index']] != 999 and ulysses_data[i,indices['quality_flag_index']] >= min_quality_flag
-    if condition:
-        ulysses_data_without_999.append(ulysses_data[i])
+class CleanedDataPlotter:
 
-ulysses_data_without_999 = np.array(ulysses_data_without_999)
+    def execute(self):
+        self._remove_999_values()
+        self._plot_hist()
 
-plt.hist(ulysses_data_without_999[:,indices['velocity_index']])
-plt.show()
+    def _remove_999_values(self):
+        ulysses_data_without_999 = []
+        for i in range(self.start_index, self.end_index):
+            condition = self.data[i,self.rotation_angle_index] != 999 and self.data[i,self.quality_flag_index] >= self.min_quality_flag
+            if condition:
+                ulysses_data_without_999.append(self.data[i])
+        self.data_without_999 = np.array(ulysses_data_without_999)
+
+    def _plot_hist(self):
+        plt.xlabel(self.xlabel)
+        plt.ylabel('Count')
+        plt.title(self.current_year)
+        plt.hist(self.data_without_999[:,self.plot_index], bins = self.bins)
+        plt.show()
+
+    def __init__(self):
+        self.data = None
+        self.start_index = None
+        self.end_index = None
+        self.min_quality_flag = None
+        self.rotation_angle_index = None
+        self.quality_flag_index = None
+        self.data_without_999 = None
+        self.xlabel = None
+        self.plot_index = None
+        self.bins = None
+        self.current_year = None
+
+LinesPlotter = CleanedDataPlotter()
+LinesPlotter.data = ulysses_data
+LinesPlotter.start_index = start_index
+LinesPlotter.end_index = end_index 
+LinesPlotter.min_quality_flag = min_quality_flag
+LinesPlotter.rotation_angle_index = indices['rotation_angle_index']
+LinesPlotter.quality_flag_index = indices['quality_flag_index']
+LinesPlotter.xlabel = PlottedQuantity
+LinesPlotter.plot_index = indices[PlottedQuantity+'_index']
+LinesPlotter.bins = bins
+
+i = 1990
+while True:
+    LinesPlotter.current_year = i
+    LinesPlotter.execute()
+    if LinesPlotter.end_index == len(ulysses_data)-1:
+        break
+    start_et = end_et
+    end_et = end_et+one_year_et
+    LinesPlotter.start_index = np.argmin(np.abs(time-start_et))
+    LinesPlotter.end_index = np.argmin(np.abs(time-end_et))
+    i += 1
